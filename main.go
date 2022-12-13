8219f914bc2755deb3f8303b790c63173d20aa32
minhducbk2010 <minhducbk2010@gmail.com>
2022-12-13 23:13:58 +0900

    create test case

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
+
+func main() {
+	PrintTeamNumber(100)
+}
 
 //	func main() {
 //		println("hello world")
@@ -15,4 +20,19 @@ func PrintAPlusB() {
 	fmt.Print("b=")
 	fmt.Scanln(&b)
 	fmt.Print("a+b=", a+b)
+}
+
+// Define PrintTeamNumber
+func PrintTeamNumber(number int) string {
+	ValueAfterConvert := strconv.Itoa(number)
+	return "Thanh vien nhom" + ValueAfterConvert
+}
+func PrintAddress(Number int, Address string) string {
+	ValueAfterConvert1 := strconv.Itoa(Number)
+	return "Dia chi:" + Address + "So nha:" + ValueAfterConvert1
+}
+
+func PrintDate(Date int) string {
+	ValueAfterConvert2 := strconv.Itoa(Date)
+	return "Ngay sinh" + ValueAfterConvert2
 }
--- main1.go
+++ main1.go
@@ -3,21 +3,5 @@ package main
 import "strconv"
 
 // Define main()
-func main() {
 
-}
-
-// Define PrintTeamNumber
-func PrintTeamNumber(number int) string {
-	ValueAfterConvert := strconv.Itoa(number)
-	return "Thanh vien nhom" + ValueAfterConvert
-}
-func PrintAddress(Number int, Address string) string {
-	ValueAfterConvert1 := strconv.Itoa(Number)
-	return "Dia chi:" + Address + "So nha:" + ValueAfterConvert1
-}
-
-func PrintDate(Date int) string {
-	ValueAfterConvert2 := strconv.Itoa(Date)
-	return "Ngay sinh" + ValueAfterConvert2
-}
+/
